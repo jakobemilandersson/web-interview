70aee9a998ba698eeaaa473a0361ab8d224ca595
Jakob Andersson <jakobemilandersson@gmail.com>
2025-08-26 20:56:46 +0200

    add: todoList#index 'db-query'

--- backend/src/models/todoList.js
+++ backend/src/models/todoList.js
@@ -1,16 +1,6 @@
-const MOCK_DATA = {
-    "0000000001": {
-      "id": "0000000001",
-      "title": "First List",
-      "todos": ["First todo of first list!"]
-    },
-    "0000000002": {
-      "id": "0000000002",
-      "title": "Second List",
-      "todos": ["First todo of second list!"]
-    }
-}
+import fs from 'fs';
 
 export function getAll() {
-    return MOCK_DATA
+    const data = fs.readFileSync('./src/database.json', 'utf8');
+    return JSON.parse(data);
 }
