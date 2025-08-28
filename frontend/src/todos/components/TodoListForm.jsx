24feef1f9d33448fd530f4b35c29821a948afb99
Jakob Andersson <jakobemilandersson@gmail.com>
2025-08-28 20:39:55 +0200

    add: todo deadline time diff

--- frontend/src/todos/components/TodoListForm.jsx
+++ frontend/src/todos/components/TodoListForm.jsx
@@ -1,17 +1,51 @@
+import { useEffect, useState } from 'react'
 import { TextField, Card, CardContent, CardActions, Button, Typography, Checkbox } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import AddIcon from '@mui/icons-material/Add'
-import { blue, lightGreen } from '@mui/material/colors'
+import { blue, lightGreen, grey } from '@mui/material/colors'
 
-const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+const ONE_MINUTE_IN_MS = 60 * 1000
+const ONE_HOURS_IN_MS = 60 * ONE_MINUTE_IN_MS
+const ONE_DAY_IN_MS = 24 * ONE_HOURS_IN_MS
 
 export const TodoListForm = ({ todoList, saveTodoList, onTodosChange }) => {
+  const [timeLeftMap, setTimeLeftMap] = useState([])
   const todos = todoList.todos
   const allDone = todos.every(todo => todo.done)
+
+  useEffect(() => {
+    const updateTimeLeft = () => setTimeLeftMap(todos.map((todo) => getTimeLeftText(todo.deadline)))
+
+    updateTimeLeft()
+    const interval = setInterval(updateTimeLeft, ONE_MINUTE_IN_MS)
+
+    return () => clearInterval(interval)
+  }, [todos])
 
   const handleSubmit = (event) => {
     event.preventDefault()
     saveTodoList(todoList.id, { todos })
+  }
+
+  const getTimeLeftText = (deadline) => {
+    const diffInMs = new Date(deadline) - new Date()
+    const isOverdue = diffInMs < 0
+    const absolutDiffInMs = Math.abs(diffInMs)
+    
+    const days = Math.floor(absolutDiffInMs / ONE_DAY_IN_MS)
+    const hours = Math.floor(absolutDiffInMs / ONE_HOURS_IN_MS) % 24
+    const minutes = Math.floor(absolutDiffInMs / ONE_MINUTE_IN_MS) % 60
+    
+    const timeParts = [
+      days && `${days}d`,
+      hours && `${hours}h`,
+      minutes && `${minutes}m`
+    ].filter(Boolean)
+    
+    if(!timeParts.length) return ''
+
+    const timeText = timeParts.join(' ')
+    return isOverdue ? `${timeText} overdue` : `${timeText} left`
   }
 
   return (
@@ -29,68 +63,73 @@ export const TodoListForm = ({ todoList, saveTodoList, onTodosChange }) => {
           style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}
         >
           {todos.map(({ name, done, deadline }, index) => (
-            <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
-              <Checkbox
-                sx={{ 
-                  '&.Mui-checked': {
-                    color: blue[600],
-                  },
-                }}
-                checked={done}
-                onChange={(event) => {
-                  onTodosChange([
-                    ...todos.slice(0, index),
-                    { name: name, done: event.target.checked, deadline: deadline },
-                    ...todos.slice(index + 1),
-                  ])
-                }}
-              />
-              <Typography sx={{ margin: '8px' }} variant='h6'>
-                {index + 1}
-              </Typography>
-              <TextField
-                sx={{ flexGrow: 10, marginTop: '1rem' }}
-                label='What to do?'
-                value={name}
-                onChange={(event) => {
-                  // immutable update
-                  onTodosChange([
-                    ...todos.slice(0, index),
-                    { name: event.target.value, done: done, deadline: deadline },
-                    ...todos.slice(index + 1),
-                  ])
-                }}
-              />
-              <TextField
-                sx={{ flexGrow: 1, marginTop: '1rem', marginLeft: '1rem' }}
-                label="Deadline"
-                type="datetime-local"
-                defaultValue={deadline}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                onChange={(event) => {
-                  onTodosChange([
-                    ...todos.slice(0, index),
-                    { name: name, done: done, deadline: event.target.value },
-                    ...todos.slice(index + 1),
-                  ])
-                }}
-              />
-              <Button
-                sx={{ margin: '8px' }}
-                size='small'
-                color='secondary'
-                onClick={() => {
-                  onTodosChange([
-                    // immutable delete
-                    ...todos.slice(0, index),
-                    ...todos.slice(index + 1),
-                  ])
-                }}
-              >
-                <DeleteIcon />
-              </Button>
+            <div key={index} style={{ display: 'flex', flexDirection: 'column' }}>
+              <span style={{ color: grey[500], alignSelf: 'center', marginTop: '1rem' }}>
+                {timeLeftMap[index]}
+              </span>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Checkbox
+                  sx={{ 
+                    '&.Mui-checked': {
+                      color: blue[600],
+                    },
+                  }}
+                  checked={done}
+                  onChange={(event) => {
+                    onTodosChange([
+                      ...todos.slice(0, index),
+                      { name: name, done: event.target.checked, deadline: deadline },
+                      ...todos.slice(index + 1),
+                    ])
+                  }}
+                />
+                <Typography sx={{ margin: '8px' }} variant='h6'>
+                  {index + 1}
+                </Typography>
+                <TextField
+                  sx={{ flexGrow: 1, marginTop: '0.5rem' }}
+                  label='What to do?'
+                  value={name}
+                  onChange={(event) => {
+                    // immutable update
+                    onTodosChange([
+                      ...todos.slice(0, index),
+                      { name: event.target.value, done: done, deadline: deadline },
+                      ...todos.slice(index + 1),
+                    ])
+                  }}
+                />
+                <TextField
+                  sx={{ flexGrow: 1, marginTop: '0.5rem', marginLeft: '1rem' }}
+                  label='Deadline'
+                  type='datetime-local'
+                  defaultValue={deadline}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  onChange={(event) => {
+                    onTodosChange([
+                      ...todos.slice(0, index),
+                      { name: name, done: done, deadline: event.target.value },
+                      ...todos.slice(index + 1),
+                    ])
+                  }}
+                />
+                <Button
+                  sx={{ margin: '8px' }}
+                  size='small'
+                  color='secondary'
+                  onClick={() => {
+                    onTodosChange([
+                      // immutable delete
+                      ...todos.slice(0, index),
+                      ...todos.slice(index + 1),
+                    ])
+                  }}
+                >
+                  <DeleteIcon />
+                </Button>
+              </div>
             </div>
           ))}
           <CardActions>
